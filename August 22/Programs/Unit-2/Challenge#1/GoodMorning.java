493a9fbd0a86199a050201a84a81030a2f7e30ed
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-08-22 15:18:15 -0600

    "Challenge 3 code"

---
+++ August 22/Programs/Unit-2/Challenge#1/GoodMorning.java
@@ -0,0 +1,7 @@
+// Create a class to output "good morning" using a text editor and check output
+
+public class GoodMorning {
+	public static void main(String[] args) {
+		System.out.println("Good morning");
+	}
+}

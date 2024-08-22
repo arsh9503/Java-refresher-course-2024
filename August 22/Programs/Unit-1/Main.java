e37c378373a6be2212df1cd50f77b13de89ce577
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-08-22 14:42:07 -0600

    "Starting out the new java course"

---
+++ August 22/Programs/Unit-1/Main.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+
+public class Main {
+
+public static void main(String[] args){
+	System.out.println("Welcome");
+	System.out.println("Coders");
+	System.out.println("Here");
+	System.out.print("Rock on!");
+	// System.out.println("My name is " + args[0]);
+}
+}

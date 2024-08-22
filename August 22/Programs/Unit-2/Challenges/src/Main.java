9f45be913fb2d37b12a895915fedb8022ff80cbc
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-08-22 15:18:05 -0600

    "Challenge 3 code"

--- August 22/Programs/Unit-2/Challenges/src/Main.java
+++ August 22/Programs/Unit-2/Challenges/src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("Welcome to the classes of Samandeep Singh Sandhu");
+        System.out.print("Samandeep Singh Sandhu");
+        System.out.print(" is a very good boy");
     }
 
 }

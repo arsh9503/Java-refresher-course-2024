a5264e613a3f9aa99d5ba6666fb8f0558a81a113
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-08-22 20:07:09 -0600

    Understanding inputs in java

---
+++ August 22/Programs/Unit-3/UserInput/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in); //Scanner class came in java 5 --> Right mei object hota hai, left mei reference
+        String name = scanner.nextLine(); // Will take string input from the terminal
+        System.out.println("Welcome to this course, " + name);
+
+        //next() would take just the first word --> it uses space as delimiter by default
+        String firstName = scanner.next();
+        System.out.println("Your first name is " + firstName);
+
+        //Lets not use the default delimiter
+        scanner.useDelimiter("@");
+        String lastName = scanner.next();
+        System.out.println("Your last name is " + lastName);
+
+    }
+}

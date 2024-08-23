41f1ce7f6627ea33a9fcad55765950448dc21981
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-08-22 19:27:22 -0600

    Variables and data types

--- August 22/Programs/Unit-2/Variables/src/Main.java
+++ August 22/Programs/Unit-2/Variables/src/Main.java
@@ -6,5 +6,19 @@ public class Main {
         System.out.println(yourNumber);
         yourNumber = 45;
         System.out.println(yourNumber);
+
+        boolean isOld = false;
+        System.out.println(isOld);
+
+        float myFloat = 5.0f; //By default, it treats it as double use trailing f to ensure it is stored and treated as a float
+        System.out.println(myFloat);
+
+        double myDouble = 6.3784239;
+        System.out.println(myDouble);
+
+        String wishes = "Good Morning Samandeep Singh"; //You need to use double quotes for string
+        System.out.println(wishes);
+
+
     }
 }

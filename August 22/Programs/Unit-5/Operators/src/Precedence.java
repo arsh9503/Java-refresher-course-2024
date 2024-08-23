acd14debd782566c266877324cd8210730c26695
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-08-22 22:41:04 -0600

    Precedence of operators

--- August 22/Programs/Unit-5/Operators/src/Precedence.java
+++ August 22/Programs/Unit-5/Operators/src/Precedence.java
@@ -3,5 +3,6 @@ public class Precedence {
         //Order of operations
         //BODMAS / PEMDAS
         System.out.println(8-3*3/2); // Multiplication is done first --> Multiplication and division have the same precedence, so the flow will go from left to right
+        System.out.println(8/4*2/4);
     }
 }

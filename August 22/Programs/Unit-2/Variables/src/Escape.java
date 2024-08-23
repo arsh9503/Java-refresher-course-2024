93e6b01f9e34d924d5a2f87c6988001efb12017b
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-08-22 19:51:34 -0600

    Learning escape sequences in java

---
+++ August 22/Programs/Unit-2/Variables/src/Escape.java
@@ -0,0 +1,11 @@
+public class Escape {
+    public static void main(String[] args){
+        System.out.println("Hello 'World'...");
+        System.out.println("Rakesh said, \"I love you Smriti\"."); // Backslash \ is used to declare a escape sequence
+        // What if I want backslash?
+        System.out.println("C:\\documents\\saman"); //It will show only one backslash
+        System.out.println("Hello \nSamandeep"); //I want to see hello and samandeep in next line --> backslash n will help me with it, it's like enter in word
+        System.out.println("Hello\tpaaji"); // \t adds a tab
+
+    }
+}

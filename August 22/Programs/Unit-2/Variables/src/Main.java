0136f7d5388460761ca259466b8dc47534dac689
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-08-22 19:14:51 -0600

    Variables and data types

---
+++ August 22/Programs/Unit-2/Variables/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int myNumber = 10; // declaration with initialization
+        int yourNumber; //declaration
+        yourNumber = 865;
+        System.out.println(yourNumber);
+        yourNumber = 45;
+        System.out.println(yourNumber);
+    }
+}

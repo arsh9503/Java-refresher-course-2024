17678158c128736bc19ad4fac2e4c435704d462e
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-08-22 22:49:12 -0600

    Augmented Assignment Operator

---
+++ August 22/Programs/Unit-5/Operators/src/ShortHand.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ShortHand {
+    public static void main(String[] args) {
+        //ShortHand Operators OR Augmented assignment operators
+        int a = 5;
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+        a += num1; //or a = a + num1;
+        System.out.println(a);
+        int num2 = scanner.nextInt();
+        a += num2;
+        System.out.println(a);
+        int num3 = scanner.nextInt();
+        a += num3;
+        System.out.println(a);
+        int num4 = scanner.nextInt();
+        a += num4;
+        System.out.println(a);
+    }
+}

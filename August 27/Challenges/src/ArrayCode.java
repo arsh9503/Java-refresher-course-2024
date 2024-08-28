f4f4ab36354e8b784a62036bb34281ee3e6e3122
ArshdeepKaur96 <a.kaur465@mybvc.ca>
2024-08-27 23:26:59 -0600

    refactor: Add ArrayCode class and implement main method to print array elements

--- August 27/Challenges/src/ArrayCode.java
+++ August 27/Challenges/src/ArrayCode.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class ArrayCode {
+    public static void main(String[] args1){
+        int[] myArray = new int[5];
+        myArray[0] = 10;
+        myArray[1] = 15;
+        myArray[2] = 17;
+        myArray[3] = 20;
+        myArray[4] = 25;
+//        System.out.println(Arrays.toString(myArray));
+        int count = 0;
+        while(count<myArray.length){
+            System.out.println(myArray[count]);
+            count++;
+        }
+    }
 }

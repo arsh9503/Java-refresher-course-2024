2b7924772bb4b8c219d3fad77934862b81e0218b
samandeep007 <hacktechnicals@gmail.com>
2024-08-29 16:49:57 -0600

    LinkedList Implementation

---
+++ August 29/LinkedList/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        LinkedList myList = new LinkedList();
+        myList.add(13);
+        myList.add(15);
+        myList.printList();
+    }
+}
